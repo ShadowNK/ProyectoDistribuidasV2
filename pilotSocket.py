70bb02a6c0d427ab9b13f4ec0f51c7317917e58a
NicolaX Inchiglema <inchiglemanicolax@gmail.com>
2020-02-06 23:57:43 -0500

    pilotui

--- pilotSocket.py
+++ pilotSocket.py
@@ -1,7 +1,7 @@
 # GUI LIBRARIES
 import sys
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWindgets import QApplication, QDialog
+from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.uic import loadUi
 from socket import *
 
